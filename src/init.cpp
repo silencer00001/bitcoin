d6f26eb7ec346cd0a295ae80881601fa1743b8da
Pieter Wuille <pieter.wuille@gmail.com>
2014-02-11 11:58:29 -0800

    Squashed commit of the following, plus fixups to carry through the development to current master:
    commit 4790f3c823a33fae44b82ef7962372e38b1b0131
    Author: Pieter Wuille <pieter.wuille@gmail.com>
    Date:   Fri Jan 11 23:34:08 2013 +0100
    
        Add address-based index
    
        1) Maintain a salt to perturbate the address index (protection against
           collisions).
        2) Add support for address index entries in the block index, and
           maintain those if -addrindex is specified. It indexes the use of
           every >8-byte data push in output script or consumed script - or in
           case of no such push, the entire script.
        3) Add a searchrawtransactions RPC call, which can look up raw
           transactions by address.
    
    commit fd867c7dbc1b5ad971dd308069257e7db5e11ac1
    Author: Pieter Wuille <pieter.wuille@gmail.com>
    Date:   Sat Jan 12 00:48:29 2013 +0100
    
        Encapsulate CLevelDB iterators cleanly
    
    Conflicts resolved:
    	src/init.cpp
    	src/main.cpp
    	src/main.h
    	src/rpcclient.cpp
    	src/rpcserver.cpp
    	src/txdb.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -834,7 +834,7 @@ bool AppInit2(boost::thread_group& threadGroup)
     else if (nTotalCache > (nMaxDbCache << 20))
         nTotalCache = (nMaxDbCache << 20); // total cache cannot be greater than nMaxDbCache
     size_t nBlockTreeDBCache = nTotalCache / 8;
-    if (nBlockTreeDBCache > (1 << 21) && !GetBoolArg("-txindex", true))
+    if (nBlockTreeDBCache > (1 << 21) && !GetBoolArg("-txindex", true) && !GetBoolArg("-addrindex", false))
         nBlockTreeDBCache = (1 << 21); // block tree db cache shouldn't be larger than 2 MiB
     nTotalCache -= nBlockTreeDBCache;
     size_t nCoinDBCache = nTotalCache / 2; // use half of the remaining cache for coindb cache
@@ -882,6 +882,12 @@ bool AppInit2(boost::thread_group& threadGroup)
                 // Check for changed -txindex state
                 if (fTxIndex != GetBoolArg("-txindex", true)) {
                     strLoadError = _("You need to rebuild the database using -reindex to change -txindex");
+                    break;
+                }
+
+                // Check for changed -addrindex state
+                if (fAddrIndex != GetBoolArg("-addrindex", false)) {
+                    strLoadError = _("You need to rebuild the database using -reindex to change -addrindex");
                     break;
                 }
 

aa37692916083ae931a5bff4b3bbe95603d10fa1
dexX7 <ugithub@bitwatch.co>
2014-10-23 08:55:12 +0200

    Add FormatTokenAmount, ...

--- src/mastercore_parse_string.cpp
+++ src/mastercore_parse_string.cpp
@@ -10,28 +10,52 @@
 
 namespace mastercore
 {
-std::string FormatDivisibleAmount(int64_t n, bool fSign)
+std::string FormatDivisibleAmount(int64_t n)
+{
+    // Prepend sign only for negative numbers
+    bool is_negative = (n < 0);
+
+    return FormatDivisibleAmount(n, is_negative);
+}
+
+std::string FormatDivisibleAmount(int64_t n, bool sign)
 {
     // Note: not using straight sprintf here because we do NOT want
     // localized number formatting.
     int64_t n_abs = (n > 0 ? n : -n);
-    int64_t quotient = n_abs/COIN;
-    int64_t remainder = n_abs%COIN;
-    std::string str = strprintf("%d.%08d", quotient, remainder);
+    int64_t quotient = n_abs / COIN;
+    int64_t remainder = n_abs % COIN;
+    std::string str = tfm::format("%ld.%08d", quotient, remainder);
 
-    if (!fSign) return str;
+    if (!sign)
+        return str;
 
     if (n < 0)
-        str.insert((unsigned int)0, 1, '-');
+        str.insert((unsigned int) 0, 1, '-');
     else
-        str.insert((unsigned int)0, 1, '+');
+        str.insert((unsigned int) 0, 1, '+');
+
     return str;
 }
 
 std::string FormatIndivisibleAmount(int64_t n)
 {
-    std::string str = strprintf("%ld", n);
-    return str;
+    return tfm::format("%ld", n);
+}
+
+std::string FormatTokenAmount(int64_t n)
+{
+    // Default token amounts are formatted as indivisible
+    // token which aligns well with an integer input
+    return FormatIndivisibleAmount(n);
+}
+
+std::string FormatTokenAmount(int64_t n, bool divisible)
+{
+    if (divisible)
+        return FormatDivisibleAmount(n);
+
+    return FormatIndivisibleAmount(n);
 }
 
 int64_t StrToInt64(const std::string& str, bool divisible)
--- src/mastercore_parse_string.h
+++ src/mastercore_parse_string.h
@@ -7,8 +7,22 @@
 namespace mastercore
 {
 // TODO: add FormatMP() -- currently depends on isPropertyDivisible()
-std::string FormatDivisibleAmount(int64_t n, bool fSign = false);
+
+// Formats a value as divisible token amount with 8 digits.
+// Per default a minus sign is prepended, if n is negative.
+// A positive or negative sign can be enforced.
+std::string FormatDivisibleAmount(int64_t n);
+std::string FormatDivisibleAmount(int64_t n, bool sign);
+
+// Formats a value as indivisible token amount with leading
+// minus sign, if n is negative.
 std::string FormatIndivisibleAmount(int64_t n);
+
+// Formats a value as token amount and prepends a minus sign,
+// if the value is negative. Divisible amounts have 8 digits.
+// Per default n is formatted as indivisible amount.
+std::string FormatTokenAmount(int64_t n);
+std::string FormatTokenAmount(int64_t n, bool divisible);
 
 // Converts strings to 64 bit wide interger.
 // Divisible and indivisible amounts are accepted.

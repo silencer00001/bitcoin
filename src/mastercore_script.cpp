770749d7d6833c0abca85ff349514deedd27ea00
dexX7 <dexx@bitwatch.co>
2014-12-29 23:58:03 +0100

    Replace: CScript::mscore_parse(..) by GetScriptPushes(..)
    Instead of using a modified version of CScript::ToString(),
    CScript::mscore_parse(,,) was replaced by an external function,
    which collects pushed values in a script.
    
    The original state of script.h is now contained.

---
+++ src/mastercore_script.cpp
@@ -0,0 +1,23 @@
+#include "mastercore_script.h"
+
+#include "script.h"
+#include "util.h"
+
+#include <string>
+#include <vector>
+
+bool GetScriptPushes(const CScript& scriptIn, std::vector<std::string>& vstrRet, bool fSkipFirst)
+{
+    int count = 0;
+    CScript::const_iterator pc = scriptIn.begin();
+    while (pc < scriptIn.end())
+    {
+        opcodetype opcode;
+        std::vector<unsigned char> data;
+        if (!scriptIn.GetOp(pc, opcode, data))
+            return false;
+        if (0 <= opcode && opcode <= OP_PUSHDATA4)
+            if (count++ || !fSkipFirst) vstrRet.push_back(HexStr(data));
+    }
+    return true;
+}

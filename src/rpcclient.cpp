9eafafd3f092ba78544afc976aecd4ec9828d219
dexX7 <dexx@bitwatch.co>
2015-03-07 10:06:16 +0100

    [Modularization] allow to add entries to the JSON conversion table
    - expose CRPCConvertParam class
    - expose CRPCConvertTable class
    - expose CRPCConvertTable rpcCvtTable
    - add method to insert custom conversion entries

--- src/rpcclient.cpp
+++ src/rpcclient.cpp
@@ -15,13 +15,6 @@
 using namespace std;
 using namespace json_spirit;
 
-class CRPCConvertParam
-{
-public:
-    std::string methodName;            //! method whose params want conversion
-    int paramIdx;                      //! 0-based idx of param to convert
-};
-
 static const CRPCConvertParam vRPCConvertParams[] =
 {
     { "stop", 0 },
@@ -90,31 +83,27 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "prioritisetransaction", 2 },
 };
 
-class CRPCConvertTable
-{
-private:
-    std::set<std::pair<std::string, int> > members;
-
-public:
-    CRPCConvertTable();
-
-    bool convert(const std::string& method, int idx) {
-        return (members.count(std::make_pair(method, idx)) > 0);
-    }
-};
-
 CRPCConvertTable::CRPCConvertTable()
 {
     const unsigned int n_elem =
         (sizeof(vRPCConvertParams) / sizeof(vRPCConvertParams[0]));
 
     for (unsigned int i = 0; i < n_elem; i++) {
-        members.insert(std::make_pair(vRPCConvertParams[i].methodName,
-                                      vRPCConvertParams[i].paramIdx));
+        addConversion(vRPCConvertParams[i]);;
     }
 }
 
-static CRPCConvertTable rpcCvtTable;
+void CRPCConvertTable::addConversion(const CRPCConvertParam& param)
+{
+    members.insert(std::make_pair(param.methodName, param.paramIdx));
+}
+
+bool CRPCConvertTable::convert(const std::string& method, int idx)
+{
+    return (members.count(std::make_pair(method, idx)) > 0);
+}
+
+CRPCConvertTable rpcCvtTable;
 
 /** Convert strings to command-specific RPC representation */
 Array RPCConvertValues(const std::string &strMethod, const std::vector<std::string> &strParams)
--- src/rpcclient.h
+++ src/rpcclient.h
@@ -10,6 +10,42 @@
 #include "json/json_spirit_utils.h"
 #include "json/json_spirit_writer_template.h"
 
+#include <set>
+#include <string>
+#include <utility>
+
+class CRPCConvertParam
+{
+public:
+    std::string methodName;            //! method whose params want conversion
+    int paramIdx;                      //! 0-based idx of param to convert
+};
+
+class CRPCConvertTable
+{
+private:
+    std::set<std::pair<std::string, int> > members;
+
+public:
+    CRPCConvertTable();
+
+    /**
+     * Adds new conversion to the conversion table
+     * @param param conversion object
+     */
+    void addConversion(const CRPCConvertParam& param);
+
+    /**
+     * Determines if a paramter should be handled as quoted or unquoted string
+     * @param method name of the method
+     * @param idx    0-based index of the paramter
+     * @return whether the paramter should be converted
+     */
+    bool convert(const std::string& method, int idx);
+};
+
+extern CRPCConvertTable rpcCvtTable;
+
 json_spirit::Array RPCConvertValues(const std::string& strMethod, const std::vector<std::string>& strParams);
 
 #endif // BITCOIN_RPCCLIENT_H

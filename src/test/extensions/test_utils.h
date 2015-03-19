2678a6c95e341134a761bbf238e12edc65957e0a
dexX7 <dexx@bitwatch.co>
2015-03-19 04:42:40 +0100

    WIP: compare size before doing vector comparison

--- src/test/extensions/test_utils.h
+++ src/test/extensions/test_utils.h
@@ -7,8 +7,12 @@
 template <class T>
 inline bool IsEqual(const T& lhs, const T& rhs)
 {
+    if (lhs.size() != rhs.size()) {
+        return false;
+    }
+
     return (std::equal(lhs.begin(), lhs.end(), rhs.begin()) &&
-            (std::equal(rhs.begin(), rhs.end(), lhs.begin())));
+           (std::equal(rhs.begin(), rhs.end(), lhs.begin())));
 }
 
 #endif // EXTENSIONS_TEST_TEST_UTILS_H

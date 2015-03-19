ab0131c9faa856c2ae8852271b6c503a4c8f7578
dexX7 <dexx@bitwatch.co>
2015-03-19 13:49:12 +0100

    WIP: test public key generation

--- src/test/extensions/mod_pubkeys_tests.cpp
+++ src/test/extensions/mod_pubkeys_tests.cpp
@@ -1,10 +1,15 @@
+#include "test/extensions/test_utils.h"
+
 #include "extensions/core/modifications/pubkeys.h"
 
 #include "pubkey.h"
 #include "utilstrencodings.h"
 
+#include <cstdlib>
 #include <string>
 #include <vector>
+
+#include <stdio.h>
 
 #include <boost/test/unit_test.hpp>
 
@@ -13,13 +18,35 @@ BOOST_AUTO_TEST_SUITE(mod_pubkeys_tests)
 BOOST_AUTO_TEST_CASE(ecdsa_modfication_test)
 {
     // Invalid public key
-    std::string str("02777ac9576cb08fb869efd4be2ca094c55808054e2220285f3c754d59361b3007");
-    std::vector<unsigned char> vch(ParseHex(str));
-    CPubKey pubKey(vch);
+    std::string strPubKey("02777ac9576cb08fb869efd4be2ca094c55808054e2220285f3c754d59361b3007");
+    
+    // Valid, modified public key
+    std::string strPubKeyExpected("02777ac9576cb08fb869efd4be2ca094c55808054e2220285f3c754d59361b300c");
+
+    CPubKey pubKey(ParseHex(strPubKey));
+    CPubKey pubKeyExpected(ParseHex(strPubKeyExpected));
 
     BOOST_CHECK(!pubKey.IsFullyValid());
     BOOST_CHECK(ModifyEcdsaPoint(pubKey));
     BOOST_CHECK(pubKey.IsFullyValid());
+    BOOST_CHECK(IsEqual(pubKey, pubKeyExpected));
+}
+
+BOOST_AUTO_TEST_CASE(create_pubkey_test)
+{
+    // Reset random number generator
+    std::srand(1);
+
+    CPubKey pubKey;
+    std::vector<unsigned char> vchBlob = ParseHex("123456");
+
+    std::string strPubKeyExpected("031234560000000000000000000000000000000000000000000000000000000001");
+    CPubKey pubKeyExpected(ParseHex(strPubKeyExpected));
+
+    BOOST_CHECK(!pubKey.IsFullyValid());
+    BOOST_CHECK(CreatePubKey(vchBlob, pubKey));
+    BOOST_CHECK(pubKey.IsFullyValid());
+    BOOST_CHECK(IsEqual(pubKey, pubKeyExpected));
 }
 
 BOOST_AUTO_TEST_SUITE_END()

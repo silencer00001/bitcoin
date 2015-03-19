a720165d14b1a72e0351d75e824ddec125c6b9ee
dexX7 <dexx@bitwatch.co>
2015-03-19 16:15:41 +0100

    WIP: help messages for data embedding RPC calls

--- src/extensions/rpc/encoding.cpp
+++ src/extensions/rpc/encoding.cpp
@@ -41,7 +41,23 @@ Value null_data(const Array& params, bool fHelp)
     if (fHelp || params.size() != 1)
         throw std::runtime_error(
             "null_data payload\n"
-            "\nIncorrect usage ...\n"
+            "\nEmbedds a payload in OP_RETURN transaction outputs.\n"
+            "\nArguments:\n"
+            "1. payload    (string, required) The hex-encoded payload\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"payload\" : \"00000000000000010000000006dac2c0\",            (string) The payload\n"
+            "  \"txouts\" : [                                               (array of JSON objects)\n"
+            "    {\n"
+            "      \"value\" : 0.00000000,                                    (number) The output value\n"
+            "      \"asm\" : \"OP_RETURN 00000000000000010000000006dac2c0\",    (string) The script\n"
+            "      \"hex\" : \"6a1000000000000000010000000006dac2c0\"           (string) The script as hex\n"
+            "    }\n"
+            "  ]\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("null_data", "\"00000000000000010000000006dac2c0\"")
+            + HelpExampleRpc("null_data", "\"00000000000000010000000006dac2c0\"")
     );
 
     std::string strPayload = params[0].get_str();
@@ -62,7 +78,6 @@ Value null_data(const Array& params, bool fHelp)
     result.push_back(Pair("txouts", vouts));
 
     return result;
-    
 }
 
 Value bare_multisig(const Array& params, bool fHelp)
@@ -70,7 +85,31 @@ Value bare_multisig(const Array& params, bool fHelp)
     if (fHelp || params.size() != 2)
         throw std::runtime_error(
             "bare_multisig payload pubkey\n"
-            "\nIncorrect usage ...\n"
+            "\nEmbedds a payload in bare-multisig transaction outputs.\n"
+            "\nNote: the public key should be used to redeem and spent data outputs.\n"
+            "\nArguments:\n"
+            "1. payload    (string, required) The hex-encoded payload\n"
+            "1. pubkey     (string, required) The public key used for redemption\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"redeemer\" : \"pubkey\",    (string) The public key\n"
+            "  \"payload\" : \"hex\",        (string) The payload\n"
+            "  \"txouts\" : [              (array of JSON objects)\n"
+            "    {\n"
+            "      \"value\" : x.xxx,        (number) The output value\n"
+            "      \"asm\" : \"asm\",          (string) The script\n"
+            "      \"hex\" : \"hex\"           (string) The script as hex\n"
+            "    },\n"
+            "    ...\n"
+            "  ]\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("bare_multisig",
+                "\"00000000000000010000000006dac2c0\" "
+                "\"02f3e471222bb57a7d416c82bf81c627bfcd2bdc47f36e763ae69935bba4601ece\"")
+            + HelpExampleRpc("bare_multisig",
+                "\"00000000000000010000000006dac2c0\","
+                "\"02f3e471222bb57a7d416c82bf81c627bfcd2bdc47f36e763ae69935bba4601ece\"")
     );
 
     std::string strPayload = params[0].get_str();
@@ -102,7 +141,36 @@ Value obfuscated_multisig(const Array& params, bool fHelp)
     if (fHelp || params.size() < 2 || params.size() > 3)
         throw std::runtime_error(
             "obfuscated_multisig address payload ( pubkey )\n"
-            "\nIncorrect usage ...\n"
+            "\nEmbedds a payload in obfuscated bare-multisig transaction outputs.\n"
+            "\nNote: the public key should be used to redeem and spent data outputs.\n"
+            "\nIf no public key is given, and the address is in the wallet, the key is retrieved.\n"
+            "\nArguments:\n"
+            "1. address    (string, required) An address used as seed for obfuscation\n"
+            "2. payload    (string, required) The hex-encoded payload\n"
+            "3. pubkey     (string, optional) The public key used for redemption\n"
+            "\nResult:\n"
+            "{\n"
+            "  \"source\" : \"address\",     (string) The address or seed\n"
+            "  \"redeemer\" : \"pubkey\",    (string) The public key\n"
+            "  \"payload\" : \"hex\",        (string) The payload\n"
+            "  \"txouts\" : [              (array of JSON objects)\n"
+            "    {\n"
+            "      \"value\" : x.xxx,        (number) The output value\n"
+            "      \"asm\" : \"asm\",          (string) The script\n"
+            "      \"hex\" : \"hex\"           (string) The script as hex\n"
+            "    },\n"
+            "    ...\n"
+            "  ]\n"
+            "}\n"
+            "\nExamples:\n"
+            + HelpExampleCli("obfuscated_multisig",
+                "\"1CE8bBr1dYZRMnpmyYsFEoexa1YoPz2mfB\" "
+                "\"00000000000000010000000006dac2c0\" "
+                "\"02f3e471222bb57a7d416c82bf81c627bfcd2bdc47f36e763ae69935bba4601ece\"")
+            + HelpExampleRpc("obfuscated_multisig",
+                "\"1CE8bBr1dYZRMnpmyYsFEoexa1YoPz2mfB\","
+                "\"00000000000000010000000006dac2c0\","
+                "\"02f3e471222bb57a7d416c82bf81c627bfcd2bdc47f36e763ae69935bba4601ece\"")
     );
 
     std::string strAddress = params[0].get_str();

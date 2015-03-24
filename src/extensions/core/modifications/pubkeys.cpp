36f5b78e9d4e2d19a182ed207cefc05418e56dbe
dexX7 <dexx@bitwatch.co>
2015-03-24 01:38:36 +0100

    Improve payload to public key conversion
    - Payload extraction
    - Split into CreatePubKeyUnchecked and (fully valid) CreatePubKey
    - Return created public key directly
    
    In contrast to earlier, the seed byte for the ECDSA point manipulation is
    now either taken from the input data, or choosen randomly.

--- src/extensions/core/modifications/pubkeys.cpp
+++ src/extensions/core/modifications/pubkeys.cpp
@@ -10,7 +10,7 @@
 static const size_t gPacketSize = 31;
 
 /**
- * Modifies the last byte of a public key until it's a valid ECDSA point.
+ * Modifies the last byte of a public key until it's a valid ECDSA coordinate.
  *
  * @param[in,out] vchPubKey  The public key to modify
  * @return True if a valid ECDSA point was found
@@ -21,14 +21,12 @@ bool ModifyEcdsaPoint(CPubKey& vchPubKey)
     std::vector<unsigned char> vchFakeKey(vchPubKey.begin(), vchPubKey.end());
 
     // Modify the last byte until the it's a valid public key
-    while (!vchPubKey.IsFullyValid())
-    {
+    while (!vchPubKey.IsFullyValid()) {
         vchFakeKey[32] = (vchFakeKey[32] + 1) % 256;
         vchPubKey.Set(vchFakeKey.begin(), vchFakeKey.end());
 
         // Fail safe: stop if we cycled once
-        if (vchFakeKey[32] == nPubKeyNonce)
-        { return false; }
+        if (vchFakeKey[32] == nPubKeyNonce) { return false; }
     }
 
     return true;
@@ -37,11 +35,12 @@ bool ModifyEcdsaPoint(CPubKey& vchPubKey)
 /**
  * Creates a compressed public key from raw bytes.
  *
- * @param[in]  vch        The data chuck
- * @param[out] pubKeyRet  The public key
- * @return True if a fully valid public key was created
+ * It is not guaranteed that the public key represents a valid ECDSA coordinate.
+ *
+ * @param vchPayload  The payload or data chuck
+ * @return The public key
  */
-bool CreatePubKey(const std::vector<unsigned char>& vch, CPubKey& pubKeyRet)
+CPubKey CreatePubKeyUnchecked(const std::vector<unsigned char>& vchPayload)
 {
     // Vector for the public key
     std::vector<unsigned char> vchFakeKey;
@@ -52,45 +51,99 @@ bool CreatePubKey(const std::vector<unsigned char>& vch, CPubKey& pubKeyRet)
 
     // Prepare the public key
     vchFakeKey.insert(vchFakeKey.end(), nPubKeyPrefix);
-    vchFakeKey.insert(vchFakeKey.end(), vch.begin(), vch.end());
+    vchFakeKey.insert(vchFakeKey.end(), vchPayload.begin(), vchPayload.end());
     vchFakeKey.resize(33);
 
     // Create the public key
-    pubKeyRet.Set(vchFakeKey.begin(), vchFakeKey.end());
+    CPubKey pubKey(vchFakeKey.begin(), vchFakeKey.end());
 
-    // Modify last byte to form a valid ecdsa point
-    if (!ModifyEcdsaPoint(pubKeyRet))
-    { return false; }
+    return pubKey;
+}
 
-    return true;
+/**
+ * Creates a fully valid compressed public key from raw bytes.
+ *
+ * A random public key prefix (0x02, 0x03) is prepended and the last byte of the
+ * key is modified, until the public key represents a valid ECDSA coordinate.
+ *
+ * @param vchPayload  The payload or data chuck
+ * @return The public key
+ */
+CPubKey CreatePubKey(const std::vector<unsigned char>& vchPayload)
+{
+    // Use random byte as seed, if none was given
+    if (vchPayload.size() < 32) {
+        std::vector<unsigned char> vchPayloadCopy = vchPayload;
+        vchPayloadCopy.resize(32);
+        vchPayloadCopy[31] = std::rand() % 256;
+
+        return CreatePubKey(vchPayloadCopy);
+    }
+
+    CPubKey pubKey = CreatePubKeyUnchecked(vchPayload);
+    ModifyEcdsaPoint(pubKey);
+
+    return pubKey;
 }
 
 /**
  * Converts a stream of bytes into compressed public keys.
  *
- * @param[in]  vch         The raw data
+ * @param[in]  vchPayload  The payload or raw data
  * @param[out] vPubKeyRet  The public keys
- * @return True if the conversion was successful
  */
-bool ConvertToPubKeys(const std::vector<unsigned char>& vch, std::vector<CPubKey>& vPubKeyRet)
+void ConvertToPubKeys(const std::vector<unsigned char>& vchPayload, std::vector<CPubKey>& vPubKeysRet)
 {
     // [1 byte, prefix] [31 byte, packet] [1 byte, ecdsa byte]
     const size_t nLength = gPacketSize;
 
     // Slice the stream of bytes into chucks of data
-    std::vector<std::vector<unsigned char> > vvchRet;
-    Slice(vch, vvchRet, nLength);
+    std::vector<std::vector<unsigned char> > vvchChucks;
+    Slice(vchPayload, vvchChucks, nLength);
 
     // Convert each chuck into a public key
     std::vector<std::vector<unsigned char> >::const_iterator it;
-    for (it = vvchRet.begin(); it != vvchRet.end(); ++it)
-    {
-        CPubKey key;
-        if (!CreatePubKey((*it), key))
-        { return false; } // Failed to form a valid ECDSA point
+    for (it = vvchChucks.begin(); it != vvchChucks.end(); ++it) {
+        CPubKey pubKey = CreatePubKey(*it);
+        vPubKeysRet.push_back(pubKey);
+    }
+}
 
-        vPubKeyRet.push_back(key);
+/**
+ * Extracts and returns the embedded payload of a public key.
+ *
+ * The public key prefix and last byte of the public key are trimmed.
+ *
+ * @param vchPubKey  The public key
+ * @return The payload
+ */
+std::vector<unsigned char> ExtractPayload(const CPubKey& vchPubKey)
+{
+    // Trim prefix and ECDSA point manipulation byte
+    return std::vector<unsigned char>(vchPubKey.begin() + 1, vchPubKey.end() - 1);
+}
+
+/**
+ * Extracts and returns the embedded payload of the given public keys.
+ *
+ * The public key prefix and last byte of each public key are trimmed. The chucks
+ * of data are concatenated.
+ *
+ * @param vPubKeys  The public keys
+ * @return The payload
+ */
+std::vector<unsigned char> ExtractPayload(const std::vector<CPubKey>& vPubKeys)
+{
+    const size_t nPayloadSize = vPubKeys.size() * gPacketSize;
+
+    std::vector<unsigned char> vchPayload;
+    vchPayload.reserve(nPayloadSize);
+
+    std::vector<CPubKey>::const_iterator it;
+    for (it = vPubKeys.begin(); it != vPubKeys.end(); ++it) {
+        std::vector<unsigned char> vchChuck = ExtractPayload(*it);
+        vchPayload.insert(vchPayload.end(), vchChuck.begin(), vchChuck.end());
     }
 
-    return true;
+    return vchPayload;
 }
--- src/extensions/core/modifications/pubkeys.h
+++ src/extensions/core/modifications/pubkeys.h
@@ -5,11 +5,17 @@
 
 class CPubKey;
 
-/** Modifies the last byte of a public key until it's a valid ECDSA point. */
+/** Modifies the last byte of a public key until it's a valid ECDSA coordinate. */
 bool ModifyEcdsaPoint(CPubKey& vchPubKey);
 /** Creates a compressed public key from raw bytes. */
-bool CreatePubKey(const std::vector<unsigned char>& vch, CPubKey& pubKeyRet);
+CPubKey CreatePubKeyUnchecked(const std::vector<unsigned char>& vchPayload);
+/** Creates a fully valid compressed public key from raw bytes. */
+CPubKey CreatePubKey(const std::vector<unsigned char>& vchPayload);
 /** Converts a stream of bytes into compressed public keys. */
-bool ConvertToPubKeys(const std::vector<unsigned char>& vch, std::vector<CPubKey>& vPubKeyRet);
+void ConvertToPubKeys(const std::vector<unsigned char>& vchPayload, std::vector<CPubKey>& vPubKeysRet);
+/** Extracts and returns the embedded payload of a public key. */
+std::vector<unsigned char> ExtractPayload(const CPubKey& vchPubKey);
+/** Extracts and returns the embedded payload of the given public keys. */
+std::vector<unsigned char> ExtractPayload(const std::vector<CPubKey>& vPubKeys);
 
 #endif // EXTENSIONS_CORE_MODIFICATIONS_PUBKEYS_H

a0cac0701542529e12d654ff28c728b69ab64177
dexX7 <dexx@bitwatch.co>
2015-04-01 19:40:33 +0200

    First take on data extraction and decoding

--- src/extensions/core/decoding.cpp
+++ src/extensions/core/decoding.cpp
@@ -13,3 +13,60 @@
 
 #include <string>
 #include <vector>
+
+/**
+ * Extracts embedded payloads from bare multisig transaction outputs.
+ *
+ * @param vTxOuts  The obfuscated transaction outputs
+ * @param vchPayloadRet The extracted payload
+ * @return True if it was successful
+ */
+bool DecodeBareMultisig(const std::vector<CTxOut>& vTxOuts, std::vector<unsigned char>& vchPayloadRet)
+{
+    // TODO: this is very inconvenient
+    // TODO: fail hard or continue, if not multisig?
+    for (std::vector<CTxOut>::const_iterator it = vTxOuts.begin(); it != vTxOuts.end(); ++it) {
+        txnouttype outtype;
+        if (!GetOutputType(it->scriptPubKey, outtype))
+            return false;
+
+        if (outtype != TX_MULTISIG)
+            return false;
+
+        std::vector<std::vector<unsigned char> > vvchChucks;
+        if (!GetScriptPushes(it->scriptPubKey, vvchChucks))
+            return false;
+
+        // Skip first public key!
+        for (size_t n = 1; n < vvchChucks.size(); ++n) {
+            CPubKey pubKey(vvchChucks[n].begin(), vvchChucks[n].end());
+            std::vector<unsigned char> vchChuck = ExtractPayload(pubKey);
+            vchPayloadRet.insert(vchPayloadRet.end(), vchChuck.begin(), vchChuck.end());
+        }
+    }
+
+    return true;
+}
+
+/**
+ * Extracts and deobfuscates embedded payloads from bare multisig transaction outputs.
+ *
+ * @param strSeed  The seed used for the deobfuscation
+ * @param vTxOuts  The obfuscated transaction outputs
+ * @param vchPayloadRet The extracted payload
+ * @return True if it was successful
+ */
+bool DecodeBareMultisigObfuscated(const std::string& strSeed, const std::vector<CTxOut>& vTxOuts,
+            std::vector<unsigned char>& vchPayloadRet)
+{
+    std::vector<unsigned char> vchPayload;
+    if (!DecodeBareMultisig(vTxOuts, vchPayload))
+        return false;
+
+    DeobfuscateUpperSha256In(vchPayload, strSeed);
+    RemoveSequenceNumbersIn(vchPayload);
+
+    vchPayloadRet.insert(vchPayloadRet.end(), vchPayload.begin(), vchPayload.end());
+
+    return true;
+}
--- src/extensions/core/decoding.h
+++ src/extensions/core/decoding.h
@@ -1,10 +1,14 @@
 #ifndef EXTENSIONS_CORE_DECODING_H
 #define EXTENSIONS_CORE_DECODING_H
 
-class CTxOut;
-
+#include <string>
 #include <vector>
 
+class CTxOut;
 
+/** Extracts embedded payloads from bare multisig transaction outputs. */
+bool DecodeBareMultisig(const std::vector<CTxOut>& vTxOuts, std::vector<unsigned char>& vchPayloadRet);
+/** Extracts and deobfuscates embedded payloads from bare multisig transaction outputs. */
+bool DecodeBareMultisigObfuscated(const std::string& strSeed, const std::vector<CTxOut>& vTxOuts, std::vector<unsigned char>& vchPayloadRet);
 
 #endif // EXTENSIONS_CORE_DECODING_H

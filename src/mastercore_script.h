96a42ad69861211a2badaa9fd4d5dd4945bf684e
dexX7 <dexx@bitwatch.co>
2015-04-08 04:32:44 +0200

    Copy Solver, ExtractDestination and ExtractDestinations

--- src/mastercore_script.h
+++ src/mastercore_script.h
@@ -1,11 +1,19 @@
 #ifndef MASTERCOIN_SCRIPT_H
 #define MASTERCOIN_SCRIPT_H
 
+class CScript;
+
+#include "script/standard.h" // for CTxDestination + txnouttypetxnouttype
+
+#include <boost/variant.hpp>
+
 #include <string>
 #include <vector>
 
-class CScript;
-
 bool GetScriptPushes(const CScript& scriptIn, std::vector<std::string>& vstrRet, bool fSkipFirst = false);
+
+bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::vector<unsigned char> >& vSolutionsRet);
+bool ExtractDestination(const CScript& scriptPubKey, CTxDestination& addressRet);
+bool ExtractDestinations(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<CTxDestination>& addressRet, int& nRequiredRet);
 
 #endif // MASTERCOIN_SCRIPT_H
